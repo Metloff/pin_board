9d89db4970b65a9e0dce2a2f8b39e0bd33a899bc
Metloff <genrih.metlov@gmail.com>
2015-12-24 13:46:23 +0300

    Pins CRUD Ability

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,52 @@
+class PinsController < ApplicationController
+  before_action :find_pin, except: [:index, :new, :create]
+
+  def index
+    @pins = Pin.order('created_at DESC')
+  end
+
+  def show
+  end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      flash[:success] = 'Pin created!'
+      redirect_to @pin
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+      flash[:success] = 'Pin was Successfuly updated'
+      redirect_to @pin
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+end
