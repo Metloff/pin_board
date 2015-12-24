a227dc759de088a135f9346880bebe7efda3cf18
Metloff <genrih.metlov@gmail.com>
2015-12-24 18:02:54 +0300

    Add and configure acts_as_votable

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :pins
+  resources :pins do
+    member do
+      put 'like', to: 'pins#upvote'
+    end
+  end
 
   root 'pins#index'
   # You can have the root of your site routed with "root"
